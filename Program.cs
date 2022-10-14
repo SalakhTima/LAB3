e865ddc913e1656854ea1fc2b04bc8498de118a9
User <User@DESKTOP-MANCTJR>
2022-10-14 20:09:09 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace projectname
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите x:");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите y:");
+                double y = Convert.ToDouble(Console.ReadLine());
+                int R = 1;
+
+                if ((x >= 0) && (y <= 0) && (x * x + y * y <= R * R))
+                {
+                    Console.WriteLine("Попадание");
+                }
+                else
+                {
+                    Console.WriteLine("Промах");
+                }
+
+            }
+        }
+    }
+}
